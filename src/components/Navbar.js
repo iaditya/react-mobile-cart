d2aabd8a6547ca70a3ad3aec562e8aa561a04131
Aditya Gupta <iadityapansari@gmail.com>
2019-09-03 12:48:42 +0530

    adds styled-components implement this in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import logo from '../logo.svg';
+import { ButtonConainer } from './Button';
 
 export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-dark bg-primary navbar-expend-sm px-sm-5">
+            <NavWrapper className="navbar navbar-dark navbar-expend-sm px-sm-5">
                 <Link to="/">
                     <img src={logo} className="navbar-brand" alt="store"></img>
                 </Link>
@@ -18,12 +20,22 @@ export default class Navbar extends Component {
                 </ul>
 
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fas fa-cart-plus"> My Cart</i>
-                    </button>
+                    <ButtonConainer >
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus"> my cart</i>
+                        </span>
+                    </ButtonConainer>
                 </Link>
-            </nav>
+            </NavWrapper>
 
         );
     }
 }
+
+const NavWrapper = styled.nav `
+background: var(--mainBlue);
+.nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+}
+`
