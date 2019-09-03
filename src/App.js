bb55eb3591aabc9053d5831ed5aadb19a13ec089
Aditya Gupta <iadityapansari@gmail.com>
2019-09-03 11:18:52 +0530

    init components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">one</div>
-        <div className="col-6">two <span className="fa fa-home"></span></div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
+      {/* <div style={{background: "#efefef"}} className="container">
+        <div className="row">
+          <div className="col-md">
+            One of three columns
+            </div>
+          <div className="col-md">
+            One of three columns
+          </div>
+          <div className="col-md">
+            One of three columns
+          </div>
+        </div>
+      </div> */}
+    </React.Fragment>
   );
 }
 
